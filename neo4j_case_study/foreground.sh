ded6229247729c238debd2f209e44e7130703dea
AlexanderHaan <haan.alex@outlook.de>
2022-03-17 18:50:08 +0100

    work

--- neo4j_case_study/foreground.sh
+++ neo4j_case_study/foreground.sh
@@ -9,11 +9,10 @@ docker run \
     -p 7474:7474 \
     -p 7687:7687 \
     --name neo4j \
-    -d \
     neo4j
 
 sleep 10
 
-docker exec -d neo4j /bin/bash /var/lib/neo4j/import_data.sh
+docker exec neo4j /bin/bash /var/lib/neo4j/import_data.sh
 
 sleep 10

c7b479b6e0caa68c735bb21ba5fd0d05080936e3
AlexanderHaan <haan.alex@outlook.de>
2022-03-17 19:12:32 +0100

    work

--- neo4j_case_study/foreground.sh
+++ neo4j_case_study/foreground.sh
@@ -4,9 +4,5 @@ docker run \
     --name neo4j \
     -p7474:7474 -p7687:7687 \
     -d \
-    -v $HOME/neo4j/data:/data \
-    -v $HOME/neo4j/logs:/logs \
-    -v $HOME/neo4j/import:/var/lib/neo4j/import \
-    -v $HOME/neo4j/plugins:/plugins \
-    --env NEO4J_AUTH=neo4j/test \
+    --env NEO4J_AUTH=none \
     neo4j:latest
